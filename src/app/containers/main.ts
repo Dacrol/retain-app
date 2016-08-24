38c82178df707779ad4f9bc21e0fd75318df54e4
Dacrol <jonas.ahrens@gmail.com>
2016-08-24 15:55:57 +0200

    Module 3 done

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -3,11 +3,15 @@
  */
 
 import { Component } from '@angular/core';
-
+import { AppBar } from '../ui';
 @Component({
     selector: 'main-container',
+    directives: [
+        AppBar
+    ],
     template: `
     <div>
+        <app-bar></app-bar>
         <main class="main">
         Content here
         </main>
