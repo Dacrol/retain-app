1c20ddde207467d7fa4a6f9e1913c99339acaa63
Dacrol <jonas.ahrens@gmail.com>
2016-08-24 14:43:06 +0200

    Module 2-1

---
+++ src/app/containers/main.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by Dacrol on 2016-08-24.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-container',
+    template: `<div>
+        <main class="main">
+        Content here
+        </main>
+    </div>
+`
+})
+
+export class Main {}/**
+ * Created by Dacrol on 2016-08-24.
+ */
---
+++ src/app/index.ts
@@ -0,0 +1,4 @@
+/**
+ * Created by Dacrol on 2016-08-24.
+ */
+export { App } from './app';
